003101ca1639e346361ed5a36b8b0aa0ddf2feb1
Joao Copertino <copertingab@gmail.com>
2024-02-01 20:48:35 -0300

    mensagem

--- js-curso-2-aula_4/app.js
+++ js-curso-2-aula_4/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -58,7 +58,7 @@ function limparCampo() {
 }
 
 function reiniciarJogo() {
-    numeroSecreto = gerarNumeroAleatorio();
+    numeroSecreto = gerarNumeroAleatorio(); 
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
